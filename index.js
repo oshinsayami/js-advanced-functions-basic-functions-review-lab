f56a0bc4ad6424652c3200f12c97efd0c2bb9830
Simran <mdrsimran@gmail.com>
2021-02-09 11:57:09 -0600

    commit tree 5f4bdf6bf80322bcbe5b0b376aada1e15c7f82d8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(string="roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string = "go to the office"){
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(character="*"){
+    return function(string = "special"){
+        return `You are ${character}${string}${character}!`
+    }
+
+}
+
+const Calculator = {
+    add: function(a, b){
+        return a + b;
+    },
+    subtract: function(a,b){
+        return a - b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    }
+}
+
+function actionApplyer(int,arr){
+    let a= int
+    for(let i=0; i< arr.length; i++){
+        a= arr[i](a)
+    }
+    return a
+}
